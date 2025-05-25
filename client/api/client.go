468c1ee47ceb76c4244d1f23535cb79e8db1facf
Ken Utsunomiya <kenutsunomiya08@gmail.com>
2025-05-25 13:10:56 +0900

    feat: client

---
+++ client/api/client.go
@@ -0,0 +1,42 @@
+package api
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type Client struct {
+	BaseURL    string
+	HTTPClient *http.Client
+}
+
+func NewClient(baseURL string) *Client {
+	return &Client{BaseURL: baseURL, HTTPClient: http.DefaultClient}
+}
+
+func (c *Client) post(ctx context.Context, path string, reqBody, respBody interface{}) error {
+	buf := new(bytes.Buffer)
+	if err := json.NewEncoder(buf).Encode(reqBody); err != nil {
+		return err
+	}
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, c.BaseURL+path, buf)
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	res, err := c.HTTPClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+	if res.StatusCode < 200 || res.StatusCode >= 300 {
+		return fmt.Errorf("HTTP %d", res.StatusCode)
+	}
+
+	return json.NewDecoder(res.Body).Decode(respBody)
+}
---
+++ client/go.mod
@@ -0,0 +1,3 @@
+module github.com/my-auth/client
+
+go 1.24.0
